6668ab809e8be747c0f6dcf804da6f3fc9b6a7eb
TecDeveloper <67659592+TecDeveloper@users.noreply.github.com>
2022-06-29 00:23:10 +0300

    Update

--- server.js
+++ server.js
@@ -42,6 +42,42 @@ app.post('/mysql', function (req,res) {
   } else {
     res.send("User couldn't be found")
   }
+  })const express = require("express")
+const app = express()
+const mysql = require("mysql")
+const cr = require("./credentials.json")
+app.get('/', function (req,res) {
+  res.send("Parameters: query,username,password. Use POST /mysql.")
+})
+let connection = mysql.createConnection({
+    host     : 'creation-db.ca6wphtfe434.me-south-1.rds.amazonaws.com',
+    user     : 'phone',
+    password : process.env.PASS,
+    database : 'main'
+  });
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!"); 
+})
+app.post('/mysql', function (req,res) {
+  let usr = req.query.username
+  let pass = req.query.password
+  let reqquery = req.query.query
+  if (usr === cr.username) {
+    if (pass === cr.password) {
+      connection.query(`${reqquery}`, function (err,resu) {
+      if (err) throw err;
+        res.send({
+  result: `${resu}`
+        })})
+    } else {
+      res.send("Wrong password")
+      }
+  } else {
+    res.send("User couldn't be found")
+  }
   })
+
+app.listen(3000)
 
 app.listen(3000)
